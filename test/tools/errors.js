fe9cfa59c10ed108c7d768dbbcfc607579b4df01
Germán Lugo <german.lugo@bitnibs.com>
2019-06-18 13:53:59 +0200

    Add a new Error.

--- test/tools/errors.js
+++ test/tools/errors.js
@@ -4,5 +4,6 @@ module.exports = { "1": "Returned error: VM Exception while processing transacti
                    "4": "Returned error: VM Exception while processing transaction: revert This function only work if you have a overcame bid -- Reason given: This function only work if you have a overcame bid.",
                    "5": "Returned error: VM Exception while processing transaction: revert Auction isn't ended yet -- Reason given: Auction isn't ended yet.",
                    "6": "Returned error: VM Exception while processing transaction: revert Is too late to make a bid. The auction has ended -- Reason given: Is too late to make a bid. The auction has ended.",
-                   "7": "Returned error: VM Exception while processing transaction: revert You don't have any funds on this contract -- Reason given: You don't have any funds on this contract."
+                   "7": "Returned error: VM Exception while processing transaction: revert You don't have any funds on this contract -- Reason given: You don't have any funds on this contract.",
+                   "8": "Returned error: VM Exception while processing transaction: revert Only beneficiary can call this function -- Reason given: Only beneficiary can call this function."
                  };
